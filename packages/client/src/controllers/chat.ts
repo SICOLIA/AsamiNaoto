a2cfaa44344bda1ec7f381c73fd95ee5c0f4504e
SICOLIA <daisukeharada0408@gmail.com>
2026-07-08 08:27:59 +0000

    fix: add background to chat notifications and make them dismissible

--- packages/client/src/controllers/chat.ts
+++ packages/client/src/controllers/chat.ts
@@ -14,7 +14,6 @@ export default class ChatController {
 
     private readonly fadingDuration = 5000;
     private fadingTimeout!: number | undefined;
-    private notifying = false;
 
     public constructor(private game: Game) {
         this.button.addEventListener('click', () => this.toggle());
@@ -65,8 +64,6 @@ export default class ChatController {
         element.style.color = colour || 'white';
         if (notify) element.style.fontWeight = 'bold';
 
-        this.notifying = !this.inputVisible();
-
         this.display();
 
         // Start the timeout for hiding the chatbox.
@@ -108,14 +105,6 @@ export default class ChatController {
 
     public toggle(): void {
         this.clearTimeout();
-
-        // If a notification is currently lingering on screen, the first press
-        // of the chat button simply dismisses it instead of opening the input.
-        if (this.notifying) {
-            this.notifying = false;
-            Util.fadeOut(this.chatBox);
-            return;
-        }
 
         if (this.inputVisible()) this.hide();
         else this.display(true);
@@ -196,10 +185,7 @@ export default class ChatController {
         this.clearTimeout();
 
         this.fadingTimeout = window.setTimeout(() => {
-            if (!this.inputVisible()) {
-                Util.fadeOut(this.chatBox);
-                this.notifying = false;
-            }
+            if (!this.inputVisible()) Util.fadeOut(this.chatBox);
         }, this.fadingDuration);
     }
 
