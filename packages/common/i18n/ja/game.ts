267dd00db536f5d1fc32b2d3db9d4516a6b1d8b1
SICOLIA <daisukeharada0408@gmail.com>
2026-07-13 05:32:07 +0000

    feat: rename game to シコリアクエスト

--- packages/common/i18n/ja/game.ts
+++ packages/common/i18n/ja/game.ts
@@ -1,5 +1,5 @@
 export default {
-    NAME: 'Kaetram',
+    NAME: 'シコリアクエスト',
     DESCRIPTION:
-        'Kaetramは、ゲーム開発の世界に興味がある人々を助けるために作られたオープンソースのゲームエンジンです。元々のアイデアはLittle Workshopのデモゲーム「BrowserQuest」を基にしています。素材（アセット）はそのまま引き継がれていますが、コード自体はゼロから完全に作り直されています。'
+        'シコリアクエスト。元々のアイデアはLittle Workshopのデモゲーム「BrowserQuest」を基にしています。素材（アセット）はそのまま引き継がれていますが、コード自体はゼロから完全に作り直されています。'
 } as const;
